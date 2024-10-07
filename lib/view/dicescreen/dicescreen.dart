eec431f4a89458c98718a8f530ed1d49718dca84
Netheesh197 <knetheesh@gmail.com>
2024-10-07 19:46:22 +0530

    dice image list removed

--- lib/view/dicescreen/dicescreen.dart
+++ lib/view/dicescreen/dicescreen.dart
@@ -1,59 +1,10 @@
-// import 'dart:math';
-
-// import 'package:flutter/material.dart';
-
-// class Dicescreen extends StatefulWidget {
-//   const Dicescreen({super.key});
-
-//   @override
-//   State<Dicescreen> createState() => _DicescreenState();
-// }
-
-// class _DicescreenState extends State<Dicescreen> {
-//    List dices = [
-//       "assets/images/d1.png",
-//       "assets/images/d2.png",
-//       "assets/images/d3.png",
-//       "assets/images/d4.png",
-//       "assets/images/d5.png",
-//       "assets/images/d6.png",
-//    ];
-// int? randomnumber;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: Center(
-//             child: ElevatedButton(///inkwell
-//                 style: ButtonStyle(padding: WidgetStatePropertyAll(EdgeInsets.all(0))),
-//                 onPressed: () {//ontap
-//                  randomnumber = Random().nextInt(6);
-//                  print(randomnumber);
-//                  setState(() {
-                   
-//                  });
-                  
-//                 }, child: Image.asset(
-//                     height: 200,
-//                     width: 200,
-
-//                     randomnumber==null? "assets/images/button.png" 
-//                     : dices[randomnumber!]
-//                     ),
-//                 ),
-//         ),
-//     );
-//   }
-// }
-
-
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 
 class Dicescreen extends StatefulWidget {
   const Dicescreen({super.key});
-
+ 
   @override
   State<Dicescreen> createState() => _DicescreenState();
 }
@@ -87,6 +38,48 @@ int? randomnumber;
     );
   }
 }
+
+
+// import 'dart:math';
+
+// import 'package:flutter/material.dart';
+
+// class Dicescreen extends StatefulWidget {
+//   const Dicescreen({super.key});
+
+//   @override
+//   State<Dicescreen> createState() => _DicescreenState();
+// }
+
+// class _DicescreenState extends State<Dicescreen> {
+   
+// int? randomnumber;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Center(
+//             child: ElevatedButton(///inkwell
+//                 style: ButtonStyle(padding: WidgetStatePropertyAll(EdgeInsets.all(0))),
+//                 onPressed: () {//ontap
+//                  randomnumber = Random().nextInt(6);
+//                  print(randomnumber);
+//                  setState(() {
+                   
+//                  });
+                  
+//                 }, child: Image.asset(
+//                     height: 200,
+//                     width: 200,
+
+//                     randomnumber==null? "assets/images/button.png" 
+//                     : "assets/images/d$randomnumber.png",
+//                     ),
+//                 ),
+//         ),
+//     );
+//   }
+// }
 
 
 
