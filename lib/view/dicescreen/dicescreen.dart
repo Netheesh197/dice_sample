de353828b95fb734f3d9b3fa3af3063baa0aab80
Netheesh197 <knetheesh@gmail.com>
2024-10-07 19:57:58 +0530

    Update dicescreen.dart

--- lib/view/dicescreen/dicescreen.dart
+++ lib/view/dicescreen/dicescreen.dart
@@ -17,6 +17,12 @@ class _DicescreenState extends State<Dicescreen> {
       "assets/images/d4.png",
       "assets/images/d5.png",
       "assets/images/d6.png",
+      "assets/images/d1.png",
+      "assets/images/d2.png",
+      "assets/images/d3.png",
+      "assets/images/d4.png",
+      "assets/images/d5.png",
+      "assets/images/d6.png",
    ];
 int? randomnumber;
 
